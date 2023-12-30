34ead9fec94af0a0fa0982084ff04c67ab7fabf0
joel_laptop <joel.hakkinen@gmail.com>
2023-12-30 15:56:53 +0200

    textarea component

---
+++ programming-ui/src/components/TextArea.svelte
@@ -0,0 +1,22 @@
+<script>
+	let text = "";
+
+	async function handleKeydown(event) {
+		if (event.key !== 'Tab') return;
+
+		event.preventDefault();
+
+    const { selectionStart, selectionEnd, value } = this;
+
+    const updatedText = value.slice(0, selectionStart) + '\t' + value.slice(selectionEnd);
+
+    text = updatedText;
+    this.selectionStart = this.selectionEnd = selectionStart + 1;
+	}
+</script>
+
+<textarea
+  class="w-full h-52 shadow-lg border border-black rounded-md font-mono"
+  bind:value={text}
+  on:keydown={handleKeydown}
+/>
