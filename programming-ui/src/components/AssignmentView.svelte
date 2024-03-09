f6e72d417eab2549c5547cbdf7c47fb12d8d8a4b
Joel Häkkinen <joel.hakkinen@gmail.com>
2024-03-09 14:10:59 +0200

    display error feedback to the client

--- programming-ui/src/components/AssignmentView.svelte
+++ programming-ui/src/components/AssignmentView.svelte
@@ -9,6 +9,8 @@
   let code = "";
   let assignment;
   let source;
+  let showFeedback = false;
+  let feedback = "";
 
   onMount(() => {
     source = new EventSource(`/sse/?user=${$userUuid}`);
@@ -21,6 +23,8 @@
         return;
       }
       alert("incorrect!");
+      showFeedback = true;
+      feedback = obj.grader_feedback;
     });
 
     source.addEventListener("init", (event) => {
@@ -35,6 +39,30 @@
   onDestroy(() => {
     source.close();
   });
+
+  const hideFeedback = () => {
+    console.log("error closed")
+    showFeedback = false;
+    feedback = "";
+  };
+
+  const submitCode = async () => {
+    const data = {
+      assignmentNumber: assignment?.assignment_order,
+      code,
+    };
+
+    const response = await fetch("/api/submissions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": $userUuid,
+      },
+      body: JSON.stringify(data),
+    });
+
+    console.log(`submission ${response.status === 200 ? "accepted": "rejected"}`);
+  };
 </script>
 
 <div class="flex flex-col gap-2 items-start">
@@ -47,6 +75,22 @@
     <p>{assignment.handout}</p>
   {/if}
 
+  {#if showFeedback}
+    <code class="relative bg-slate-200 text-red-500">
+      <button
+        class="absolute -top-2 -right-2 z-10 rounded-full bg-white"
+        on:click={hideFeedback}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+      </button>
+      {feedback}
+    </code>
+  {/if}
+
   <TextArea bind:code />
-  <GradingButton code={code} assignmentNumber={assignment?.assignment_order} />
+  <GradingButton {submitCode}>
+    Submit
+  </GradingButton>
 </div>
--- programming-ui/src/components/GradingButton.svelte
+++ programming-ui/src/components/GradingButton.svelte
@@ -1,32 +1,10 @@
 <script>
-  import { userUuid } from "../stores/stores.js";
-
-  export let code;
-  export let assignmentNumber;
-  
-  const submitCode = async () => {
-    const data = {
-      assignmentNumber,
-      code,
-    };
-
-    const response = await fetch("/api/submissions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": $userUuid,
-      },
-      body: JSON.stringify(data),
-    });
-
-    console.log(`submission, ${response.status === 200 ? "accepted": "rejected"}`);
-  };
+  export let submitCode;
 </script>
 
 <button
   class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded disabled:bg-gray-500"
   on:click={submitCode}
-  disabled={!assignmentNumber}
 >
-  Submit
+  <slot />
 </button>
